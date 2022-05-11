6dadc2bee7391be9f97a7b3c5881a9de45d83df6
sheikhjaber0424 <55077645+sheikhjaber0424@users.noreply.github.com>
2022-05-11 12:20:59 +0600

    update categories

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+    $conn = mysqli_connect('localhost', 'root','','php_restaurant')
+            or die('problem connecting to database');
+
+?> 
---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<!------- Header Section ------->
+<header id="header">
+    <div class="intro">
+        <div class="overlay">
+        <div class="container">
+            <div class="row">
+                <div class="intro-text">
+                    <h1>W&S</h1>
+                    <p>Restaurant / Coffee / Breakfast</p>
+                    <a href="" class="btn btn-custom btn-lg page-scroll">Discover Our Sory</a>
+                </div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+
+</header>
+ 
+
+
+<!------- About Section ------->
+<div class="about">
+    <div style="margin-inline: 10%; margin-block: 100px">
+        <div class="row" style="display: flex; flex-direction:row">
+            <div class="col-xs-12 col-md-6">
+                <div class="about-img">
+                    <img src="img/about.jpg" style="width: 100%;"  alt="" >
+                </div>
+            </div>    
+            <div class="col-xs-12 col-md-6">
+                <div class="about-text">
+                    <h2>Our Restaurant</h2>
+                    <hr>
+                     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui accusantium at quibusdam quidem dolor ut ullam tempore vitae quasi esse quia asperiores ab, aliquam, deserunt id itaque sit laborum nobis!</p>
+                    <h3>Awards</h3> 
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, tempore? Quisquam architecto nam, officiis autem ex sapiente quo repudiandae ab laboriosam dolor iste illum sunt nobis accusantium atque? Eveniet, iusto.</p>
+                </div>
+            </div>    
+            
+        </div>
+    </div>
+</div>
+
+<!-- Restaurant Menu Section -->
+<div id="restaurant-menu">
+  <div class="section-title text-center">
+    <div class="overlay">
+      <h2>Menu</h2>
+      <hr/>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ab eos voluptates accusamus ratione, quo aliquam, esse illum nihil id odit assumenda vitae labore eligendi fuga at, dolore velit quos?</p>
+
+    </div>
+  </div>
+
+
+<div class="container">
+  <div class="row">
+    
+    <div class="col-xs-12 col-md-6">
+      <div class="menu-section">
+        <h2 class="menu-section-title">Breakfast</h2>
+        <hr/>
+        <div class="menu-item">
+          <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="col-xs-12 col-md-6">
+      <div class="menu-section">
+        <h2 class="menu-section-title">Main Meals</h2>
+        <hr/>
+        <div class="menu-item">
+          <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="col-xs-12 col-md-6">
+      <div class="menu-section">
+        <h2 class="menu-section-title">Dinner</h2>
+        <hr/>
+        <div class="menu-item">
+          <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="col-xs-12 col-md-6">
+      <div class="menu-section">
+        <h2 class="menu-section-title">Coffee/Drinks</h2>
+        <hr/>
+        <div class="menu-item">
+          <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+        <div class="menu-item">
+        <div style="display: inline-block;" class="menu-item-name">Delicious Meal</div>
+          <div style="display: inline-block; margin-left: 5%;" class="menu-item-price">$55</div>
+          <div class="menu-item-description">Lorem ipsum dolor!</div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<!-- Gallery -->
+<div id="portfolio">
+  <div class="section-text text-center">
+    <div class="overlay">
+      <h2>Gallery</h2>
+      <hr>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit!</p>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="categories">
+        <ul class="cat">
+            <li>
+              <ol class="type">
+              <li ><a href="" class="active">All</a></li>
+              <li ><a href="" >Breakfast</a></li>
+              <li ><a href="" >Dinner</a></li>
+              <li ><a href="" >Coffee</a></li>
+               
+              </ol>
+            </li>
+        </ul>
+        <fiv class="clearfix"></fiv>
+      </div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg">
+              <div class="hover-text">
+                <h4>Meal Name</h4>
+              </div>
+              <div class="hover-text">
+                <h4>Meal Name</h4>
+              </div>
+              <div class="hover-text">
+                <h4>Meal Name</h4>
+              </div>
+              <img src="" class="img-responsive" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
